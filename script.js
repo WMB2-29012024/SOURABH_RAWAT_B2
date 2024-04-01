3249c1eb1ae07a998918df18fe8b216d5a43909a
Sourabh10122002 <souravrawat444@gmail.com>
2024-04-01 21:42:11 +0530

    Compl

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const input = document.getElementById("text");
 const button = document.getElementById("button");
 const para = document.getElementById("result");
-const newPara = document.createElement("p");
 
 button.addEventListener("click", (e) => {
     e.preventDefault();
+    para.innerText="";
     const newPara = document.createElement("p");
     newPara.textContent = `Hi, ${input.value}`;
     para.appendChild(newPara);
