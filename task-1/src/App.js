5e4d67dcbab0ed3a7cf2b2f3d218a670bc5d7bc0
Sourabh10122002 <souravrawat444@gmail.com>
2024-05-05 20:47:06 +0530

    task-1 completed

---
+++ task-1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UserInfo from './UserInfo';
+
+function App() {
+  return (
+    <div className="App">
+      <UserInfo userName={"Sourabh Rawat"} />
+    </div>
+  );
+}
+
+export default App;
---
+++ task-1/src/UserInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserInfo = ({ userName }) => {
+    return (
+        <div>
+            <h1>Hello <span style={{ color: 'red' }}>{userName}</span>,</h1>
+        </div>
+    )
+}
+
+export default UserInfo
